1ddc9f0b74e7316bffe207c2385e910e084bfe40
Johnlali <ioannislali@gmail.com>
2025-05-17 19:06:58 +0300

    Add JS site

---
+++ index.js
@@ -0,0 +1,10 @@
+<html>
+<html lang="en"></html>
+<head>
+  <meta charset="UTF-8"> </meta>
+  <title>Kontouzi core</title>
+</head>
+<body>
+<text>Gamao Giagiades ahhhhhhhhhhhhhhhhhhh</text> 
+</body>
+</html>
